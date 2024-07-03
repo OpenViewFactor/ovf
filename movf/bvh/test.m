c8d69842ee2fd00c28a48194e748419f9e9f5e77
Natan Herzog <collegenatan@gmail.com>
2024-07-03 15:04:01 -0400

    changing test script interface to vary inputs

--- movf/bvh/test.m
+++ movf/bvh/test.m
@@ -2,14 +2,14 @@ clear all
 close all
 clc
 
-layer_height = 20;
-minimum_triangles_per_bbox = 2;
+filename = input("Enter the mesh filepath: ", "s");
+layer_height = input("Enter a layer height for the BVH: ");
+minimum_triangles_per_bbox = input("Enter the minimum number of triangles per BV: ");
 
-mesh = readMesh("assets/rtg_model.stl");
+mesh = readMesh(filename);
 bvh = BVH;
 bvh.linkToMesh(mesh);
 bvh.setLayerHeight(layer_height);
 bvh.setMinimumNumTriangles(minimum_triangles_per_bbox);
 bvh.constructBVH;
-bvh.plotMesh;
 bvh.plotLeafNodes;

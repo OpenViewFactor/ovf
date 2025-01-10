a17d839b75e3f2140ba284ff1816dd6880f93e45
Natan Herzog <collegenatan@gmail.com>
2025-01-10 16:04:35 -0500

    adding nan catch. didn't do anything though

--- ovf/solvers/src/DoubleAreaIntegration.cpp
+++ ovf/solvers/src/DoubleAreaIntegration.cpp
@@ -4,8 +4,12 @@ namespace openviewfactor {
   template <typename FLOAT_TYPE>
   OVF_HOST_DEVICE FLOAT_TYPE DoubleAreaIntegration<FLOAT_TYPE>::solveViewFactorBetweenElements(const Vector3<FLOAT_TYPE>& emitter_centroid, const Vector3<FLOAT_TYPE>& emitter_normal, const Vector3<FLOAT_TYPE>& receiver_centroid, const Vector3<FLOAT_TYPE>& receiver_normal, FLOAT_TYPE receiver_area) const {
     auto ray_vector = receiver_centroid - emitter_centroid;
-    FLOAT_TYPE view_factor = (emitter_normal).dot(ray_vector) * (receiver_normal).dot(ray_vector) * receiver_area / (ray_vector.dot(ray_vector) * ray_vector.dot(ray_vector) * M_PI);
-    return view_factor;
+    auto scale_factor = 1 / (ray_vector.dot(ray_vector) * ray_vector.dot(ray_vector) * M_PI);
+    if (!isnan(scale_factor)) {
+      return ((emitter_normal).dot(ray_vector) * (receiver_normal).dot(ray_vector) * receiver_area * scale_factor);
+    } else {
+      return 0.0;
+    }
   }
 
   template class DoubleAreaIntegration<float>;

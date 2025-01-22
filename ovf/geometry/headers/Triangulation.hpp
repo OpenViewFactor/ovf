d8e603059640b83ad81651054b42459735cc00b6
NatanHerzog <natanherzog@gmail.com>
2025-01-21 21:20:14 -0500

    new changes to code for gpu stuff in new branch for gpu

--- ovf/geometry/headers/Triangulation.hpp
+++ ovf/geometry/headers/Triangulation.hpp
@@ -12,22 +12,17 @@
 
 namespace openviewfactor {
 
-enum MeshType { EMIT, RECEIVE, BLOCKER, UNKNOWN };
-
 template <typename FLOAT_TYPE> class Triangulation {
 
   private:
     std::vector<Vector3<FLOAT_TYPE>> _pts;  // points list in the triangulation
     std::vector<std::array<size_t, 3>> _con; // connectivity list in the triangulation
-    MeshType _name;  // enumeration: EMITTER, RECEIVER, BLOCKER)
   protected:
   public:
     //* ----- CLASS CONSTRUCTORS ----- *//
     OVF_HOST_DEVICE Triangulation();  // default constructor
-    OVF_HOST_DEVICE Triangulation(MeshType name);  // name constructor
 
     //* ----- ACCESSOR METHODS ----- *//
-    OVF_HOST_DEVICE MeshType getName() const;
     OVF_HOST_DEVICE size_t getNumElements() const;
     OVF_HOST_DEVICE size_t getNumPoints() const;
     OVF_HOST_DEVICE size_t getNumBytes() const;
@@ -48,7 +43,6 @@ template <typename FLOAT_TYPE> class Triangulation {
     OVF_HOST_DEVICE std::vector<Triangle<FLOAT_TYPE>> getTriangles() const;
 
     //* ----- MUTATOR METHODS ----- *//
-    OVF_HOST_DEVICE Triangulation<FLOAT_TYPE>& setName(const MeshType &name);
     OVF_HOST_DEVICE Triangulation<FLOAT_TYPE>& clear();
     OVF_HOST_DEVICE Triangulation<FLOAT_TYPE>& addElement(Vector3<FLOAT_TYPE> OA, Vector3<FLOAT_TYPE> OB, Vector3<FLOAT_TYPE> OC);
     OVF_HOST_DEVICE Triangulation<FLOAT_TYPE>& addElement(Triangle<FLOAT_TYPE> tri);
--- ovf/solvers/headers/SingleAreaIntegration.hpp
+++ ovf/solvers/headers/SingleAreaIntegration.hpp
@@ -1,18 +1,6 @@
 #ifndef _OVF_SINGLEAREAINTEGRATION_H_
 #define _OVF_SINGLEAREAINTEGRATION_H_
 
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <numeric>
-
-#include <vector>
-#include <algorithm>
-
-#include "Vector3.hpp"
-#include "Triangle.hpp"
-#include "Triangulation.hpp"
-#include "BVH.hpp"
-#include "Ray.hpp"
 #include "Solver.hpp"
 
 namespace openviewfactor {
